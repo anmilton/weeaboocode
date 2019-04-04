0b2d6e44f844c4abf00c624e8a53d27e7aef96a6
anmilton <43585134+anmilton@users.noreply.github.com>
2019-04-04 10:42:05 -0600

    dao comment fixed

--- Dao2.php
+++ Dao2.php
@@ -2,8 +2,6 @@
 require_once 'KLogger.php';
 
 class Dao {
-  I removed the db info since the repo is public and I didn't want my tables dropped
-  Hopfully this is ok!
   private $host = "";
   private $db = "";
   private $user = "";
@@ -27,32 +25,115 @@ class Dao {
     return $conn;
   }
 
-  public function getComments () {
+  public function getAnimes () {
     $conn = $this->getConnection();
-    return $conn->query("select comment, date_created  from comment order by date_created desc", PDO::FETCH_ASSOC);
+    $getQuery = "select *  from animes limit 3";
+    $q = $conn->prepare($getQuery);
+    $q->execute();
+    $results = $q->setFetchMode(PDO::FETCH_ASSOC);
+    $results = $q->fetchAll();
+    if(empty($results))
+	{
+		    $this->logger->LogInfo("No animes");
+		    return null;
+	}
+    return $results;
+  }
+
+  public function getAlphaAnimes ($letter) {
+    $conn = $this->getConnection();
+    $getQuery = "select *  from animes where title like '$letter%'";
+    $q = $conn->prepare($getQuery);
+    $q->execute();
+    $results = $q->setFetchMode(PDO::FETCH_ASSOC);
+    $results = $q->fetchAll();
+    if(empty($results))
+	{
+		    $this->logger->LogInfo("No animes");
+		    return null;
+	}
+    return $results;
+  }
+
+  public function getComments ($title) {
+    $conn = $this->getConnection();
+    $getQuery = "select *  from comments where title = :title order by datecreated desc";
+    $q = $conn->prepare($getQuery);
+    $q->bindParam(":title", $title);
+    $q->execute();
+    $results = $q->setFetchMode(PDO::FETCH_ASSOC);
+    $results = $q->fetchAll();
+    if(empty($results))
+	{
+		    $this->logger->LogInfo("No comments for $title");
+		    return null;
+	}
+    return $results;
   }
 
   public function getUser ($username) {
     $conn = $this->getConnection();
-    return $conn->query("select *  from user where user_name = {$userName}", PDO::FETCH_ASSOC);
-  }
+    $getQuery = "select *  from users where username = :username";
+    $q = $conn->prepare($getQuery);
+    $q->bindParam(":username", $username);
+    $q->execute();
+    $results = $q->setFetchMode(PDO::FETCH_ASSOC);
+    $results = $q->fetchAll();
+    if(empty($results))
+	{
+		    $this->logger->LogInfo("No user [{$username}]");
+		    return null;
+	}
+    return $results;
+    }
+
+  public function getOneAnime ($title) {
+    $conn = $this->getConnection();
+    $getQuery = "select *  from animes where title = :title";
+    $q = $conn->prepare($getQuery);
+    $q->bindParam(":title", $title);
+    $q->execute();
+    $results = $q->setFetchMode(PDO::FETCH_ASSOC);
+    $results = $q->fetchAll();
+    if(empty($results))
+	{
+		    $this->logger->LogInfo("No anime [{$title}]");
+		    return null;
+	}
+    return $results;
+    }
 
   public function createUser ($username, $password, $email) {
+    $this->logger->LogInfo("Saving a user [{$username}]");
     $conn = $this->getConnection();
-    $saveQuery = "insert into user (username, password, email) values (:username, :password, :email)";
+    $saveQuery = "insert into users (username, password, email) values (:username, :password, :email)";
     $q = $conn->prepare($saveQuery);
-    $q->bindParam(":name", $userName);
-    $q->bindParam(":email", $userName);
+    $q->bindParam(":username", $username);
     $q->bindParam(":password", $password);
+    $q->bindParam(":email", $email);
     $q->execute();
   }
 
-  public function saveComment ($comment) {
+  public function createAnime ($title, $tvrating, $genre, $description) {
+    $this->logger->LogInfo("Saving an anime [{$title}]");
+    $conn = $this->getConnection();
+    $saveQuery = "insert into animes (title, tvrating, genre, description) values (:title, :tvrating, :genre, :description)";
+    $q = $conn->prepare($saveQuery);
+    $q->bindParam(":title", $title);
+    $q->bindParam(":tvrating", $tvrating);
+    $q->bindParam(":genre", $genre);
+    $q->bindParam(":description", $description);
+    $q->execute();
+  }
+
+  public function saveComment ($comment, $username, $title) {
     $this->logger->LogInfo("Saving a comment [{$comment}]");
     $conn = $this->getConnection();
-    $saveQuery = "insert into comment (comment, user_id) values (:comment, 1)";
+    $saveQuery = "insert into comments (comment, username, title) values (:comment, :username, :title)";
     $q = $conn->prepare($saveQuery);
     $q->bindParam(":comment", $comment);
+    $q->bindParam(":username", $username);
+    $q->bindParam(":title", $title);
     $q->execute();
   }
 }
